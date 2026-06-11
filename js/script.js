fce4685514da1f08ee6b2808cd7e29add7b255e5
Sophie <dufranesophie@gmail.com>
2026-06-11 10:38:24 +0200

    refactor: map sheet headers dynamically to avoid hardcoded column indexes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const sheetID = "1uHY6r27bw8AJ-YRA1S9F7Ri_moliZ5SmT1NqnHZ3oR8";
 const apiURL = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json`;
 
 const collectionContainer = document.getElementById("collection-container");
+
 const cardColor = [
   { background: "var(--color-blue)", color: "var(--color-raisinBlack)" },
   { background: "var(--color-orange)", color: "var(--color-raisinBlack)" },
@@ -15,28 +16,32 @@ async function populateCollection() {
   const text = await response.text();
   const json = JSON.parse(text.substring(47).slice(0, -2));
   const rows = json.table.rows;
+  const headers = json.table.cols.map((col) => col.label.toLowerCase()); // const headers = ["nom", "collection", "annee", "etat",....];
 
   rows.forEach((row, index) => {
-    const nom = row.c[0].v;
-    const collection = row.c[1].v;
-    const annee = row.c[2].v;
-    const commentaire = row.c[3].v;
-    const photo = row.c[4].v;
-    const productCard = document.createElement("div");
-    const colorIndex = index % cardColor.length;
+    const data = {}; // Create an object to hold the data of the table for each row, headers are the keys and the corresponding cell values are the values of the object
+    headers.forEach((header, i) => {
+      data[header] = row.c[i]?.v;
+    });
 
-    let photoURL = photo !== null ? photo : "images/no-photo.webp";
+    const productCard = document.createElement("div");
+
+    const colorIndex = index % cardColor.length; // Cycle through the cardColor array to assign colors to the cards on a rotating basis
+
+    let photoURL =
+      data["photo"] !== undefined && data["photo"] !== null
+        ? data["photo"]
+        : "images/no-photo.webp"; // Use a default image if the photo URL is missing so data is either undefined or null
 
     productCard.classList.add("col-12", "col-md-6", "col-lg-3", "product-card");
     productCard.innerHTML = `
         <div class="card" style="background: ${cardColor[colorIndex].background}; color: ${cardColor[colorIndex].color};">
           <div class="card-image"> 
-            <img src="${photoURL}" alt="${nom}">
+            <img src="${photoURL}" alt="${data["nom"]}">
           </div>
             <div class="card-body">
-                <h5 class="card-title">${nom} - ${annee}</h5>
-                <h6 class="card-subtitle mb-2">Collection: ${collection}</h6>
-                <p class="card-text">${commentaire}</p>
+                <h5 class="card-title">${data["nom"]}</h5>
+                <p class="card-subtitle">${data["annee"]}</p>
             </div>
         </div>
     `;
